5339eb573fa688d601d551862d090e1fb103a94f
sivaram260 <76741038+sivaram260@users.noreply.github.com>
2021-01-24 17:06:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+setInterval(checkBackground,2000);
+function checkBackground(){
+generateRandomColor();
+changeBackgroundColor();
+changeBackgroundText();
+}
+
+function generateRandomColor(){
+return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+function changeBackgroundColor(){
+let colorBg=document.getElementById('color-overlay');
+colorBg.style.backgroundColor=generateRandomColor();
+}
+
+function changeBackgroundText(){
+let textbg=document.getElementById('text-bg');
+if(textbg.innerHTML=='LETS ROCK AND ROLL')
+textbg.innerHTML='ON THE FLOOR BABY';
+else if(textbg.innerHTML=='ON THE FLOOR BABY')
+textbg.innerHTML='HIT IT HARD';
+else if(textbg.innerHTML=='HIT IT HARD')
+textbg.innerHTML='ROCK THE PARTY BABY';
+else if(textbg.innerHTML=='ROCK THE PARTY BABY')
+textbg.innerHTML='LETS ROCK AND ROLL';
+}
